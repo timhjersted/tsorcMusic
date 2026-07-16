0fd75600467a0a32b07ab077858cee475fca2b84
timhjersted <88109305+timhjersted@users.noreply.github.com>
2026-07-15 23:25:05 -0500

    Added background art by Zedotagger plus changed main menu theme
    New menu theme is "Night" + also added logo
    -Removed a song and renamed another, currently not used

--- tsorcMusic.cs
+++ tsorcMusic.cs
@@ -3,6 +3,8 @@ using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 //using tsorcRevamp;
 using Terraria;
 using Terraria.Audio;
@@ -69,12 +71,84 @@ namespace tsorcMusic
     }
     public class tsorcMusicMenu : ModMenu
     {
-        public override int Music => MusicLoader.GetMusicSlot(tsorcMusic.instance, "Sounds/Music/VillageDay");
-        public override string Name => "The Story of Red Cloud";
-        public override void Update(bool isOnTitleScreen)
-        {            
+        private const int AnimationFrameCount = 36;
+        private const int AnimationFrameWidth = 960;
+        private const int AnimationFrameHeight = 512;
+        private const int AnimationAtlasColumns = 4;
+        private const int AnimationFramesPerAtlas = 20;
+        private const float AnimationSecondsPerFrame = 0.11f;
 
-           Main.curMusic = MusicLoader.GetMusicSlot(tsorcMusic.instance, "Sounds/Music/VillageDay");
+        private Asset<Texture2D> logo;
+        private Asset<Texture2D>[] backgroundAtlases;
+
+        public override int Music => MusicLoader.GetMusicSlot(tsorcMusic.instance, "Sounds/Music/Night");
+        public override string Name => "The Story of Red Cloud";
+        public override Asset<Texture2D> Logo => logo ??= ModContent.Request<Texture2D>("tsorcMusic/MainMenu/tsorc_logo", AssetRequestMode.ImmediateLoad);
+
+        public override void Load()
+        {
+            if (!Main.dedServ)
+            {
+                backgroundAtlases = new[]
+                {
+                    ModContent.Request<Texture2D>("tsorcMusic/MainMenu/praise_the_sun_background_atlas_0", AssetRequestMode.ImmediateLoad),
+                    ModContent.Request<Texture2D>("tsorcMusic/MainMenu/praise_the_sun_background_atlas_1", AssetRequestMode.ImmediateLoad)
+                };
+            }
+        }
+
+        public override void Unload()
+        {
+            logo = null;
+            backgroundAtlases = null;
+        }
+
+        public override void Update(bool isOnTitleScreen)
+        {
+            Main.curMusic = MusicLoader.GetMusicSlot(tsorcMusic.instance, "Sounds/Music/Night");
+        }
+
+        public override bool PreDrawLogo(SpriteBatch spriteBatch, ref Vector2 logoDrawCenter, ref float logoRotation, ref float logoScale, ref Color drawColor)
+        {
+            DrawMenuBackground(spriteBatch);
+
+            Texture2D logoTexture = Logo.Value;
+            float maxLogoWidth = Main.screenWidth * 0.58f;
+            float maxLogoHeight = Main.screenHeight * 0.42f;
+            logoScale = MathHelper.Min(maxLogoWidth / logoTexture.Width, maxLogoHeight / logoTexture.Height);
+            logoRotation = 0f;
+            logoDrawCenter = new Vector2(Main.screenWidth * 0.5f, Main.screenHeight * 0.18f);
+
+            return true;
+        }
+
+        private void DrawMenuBackground(SpriteBatch spriteBatch)
+        {
+            if (backgroundAtlases == null)
+            {
+                return;
+            }
+
+            int frame = (int)(Main.GlobalTimeWrappedHourly / AnimationSecondsPerFrame) % AnimationFrameCount;
+            int atlasIndex = frame / AnimationFramesPerAtlas;
+            int localFrame = frame % AnimationFramesPerAtlas;
+            Asset<Texture2D> atlas = backgroundAtlases[atlasIndex];
+            if (atlas?.IsLoaded != true)
+            {
+                return;
+            }
+
+            Rectangle source = new Rectangle(
+                localFrame % AnimationAtlasColumns * AnimationFrameWidth,
+                localFrame / AnimationAtlasColumns * AnimationFrameHeight,
+                AnimationFrameWidth,
+                AnimationFrameHeight);
+
+            float scale = MathHelper.Max((float)Main.screenWidth / AnimationFrameWidth, (float)Main.screenHeight / AnimationFrameHeight);
+            Vector2 size = new Vector2(AnimationFrameWidth, AnimationFrameHeight) * scale;
+            Vector2 position = new Vector2((Main.screenWidth - size.X) * 0.5f, (Main.screenHeight - size.Y) * 0.5f);
+
+            spriteBatch.Draw(atlas.Value, position, source, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
         }
     }
 
@@ -373,7 +447,7 @@ namespace tsorcMusic
                     }
                     else if (Main.LocalPlayer.ZoneCorrupt && Main.LocalPlayer.ZoneRockLayerHeight && Main.dayTime)
                     {
-                        Music = MusicLoader.GetMusicSlot(tsorcMusic.instance, "Sounds/Music/UndergroundCorruptionDay");
+                        Music = MusicLoader.GetMusicSlot(tsorcMusic.instance, "Sounds/Music/CorruptionUndergroundDay");
                         Priority = SceneEffectPriority.Environment;
                     }
 		  else if (Main.LocalPlayer.ZoneCorrupt && Main.LocalPlayer.ZoneOverworldHeight && !Main.dayTime)
@@ -417,10 +491,10 @@ namespace tsorcMusic
                         Music = MusicLoader.GetMusicSlot(tsorcMusic.instance, "Sounds/Music/Crimson");
                         Priority = SceneEffectPriority.BiomeMedium;
                     }
-                    // Underworld day and night.
+                    // Underworld/Hell day and night.
                     else if (Main.LocalPlayer.ZoneUnderworldHeight && Main.dayTime)
                     {
-                        Music = MusicLoader.GetMusicSlot(tsorcMusic.instance, "Sounds/Music/Underworld");
+                        Music = MusicLoader.GetMusicSlot(tsorcMusic.instance, "Sounds/Music/MetroidUndergroundDepths");
                         Priority = SceneEffectPriority.Event;
                     }
                     else if (Main.LocalPlayer.ZoneUnderworldHeight && !Main.dayTime)
